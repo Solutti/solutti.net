16d41b41bb3adb88d6ad72eda23d4758ff283851
Sali Dev <solutti.net@gmail.com>
2024-02-06 15:06:17 -0300

    Update

--- src/app/componets/sections/about.tsx
+++ src/app/componets/sections/about.tsx
@@ -1,6 +1,6 @@
 export function QuemSomos(){
     return(
-        <div className="min-h-screen w-full flex items-center justify-center" id="quemSomos">
+        <div className="min-h-screen w-full flex items-center justify-center bg-gray-dark" id="quemSomos">
             Quem Somos
         </div>
     )
--- src/app/componets/sections/presenca.tsx
+++ src/app/componets/sections/presenca.tsx
@@ -6,8 +6,8 @@ import MAP from "../../../../public/map/map.png"
 
 export function Presenca(){
     return(
-        <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-dark" id="presenca">
-            <div className=" font-sans text-base lg:text-2xl text-gray-light lg:m-24 lg:w-[50%] w-[100%] m-8 p-2 text-center pt-20">Possuímos pontos de presença no sul e sudeste do Brasil, além de conexão até Miami/USA, interligando cidades estratégicas para oferecer a melhor experiência.</div>
+        <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-dark py-20" id="presenca">
+            <div className=" font-sans text-base lg:text-2xl text-gray-light lg:m-24 lg:w-[50%] w-[100%] m-8 p-2 text-center">Possuímos pontos de presença no sul e sudeste do Brasil, além de conexão até Miami/USA, interligando cidades estratégicas para oferecer a melhor experiência.</div>
             <div className=" center flex flex-1 gap-20">
                 <div className="flex sm:w-[100%] lg:w-[50%] h-full">
                     <Image alt="imagem do mapa" className=" flex flex-1" src={MAP} />
