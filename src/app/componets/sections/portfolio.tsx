784c650d82b053639dea173eef8ac8c525a260b6
Sali Dev <solutti.net@gmail.com>
2024-02-06 15:07:18 -0300

    Update

--- src/app/componets/sections/portfolio.tsx
+++ src/app/componets/sections/portfolio.tsx
@@ -20,7 +20,7 @@ export function Portfolio(){
         <div className="min-h-screen flex items-center justify-center" id="portfolio">
             <div className="center gap-20">
                 <div className="flex flex-col w-[100%] lg:w-[50vw] gap-6 py-10">
-                    <h2 className=" text-4xl font-sans font-bold text-gray-dark" aria-label="Nossos serviços">NOSSOS SERVIÇOS</h2>
+                    <h2 className=" text-4xl font-sans font-bold text-lilas" aria-label="Nossos serviços">NOSSOS SERVIÇOS</h2>
                     <p className="text-base font-sans font-regular text-gray" aria-label="Descrição da seção nossos serviços">Cada serviço da Solutti Networks é projetado para elevar sua experiência de conectividade, proporcionando eficiência, segurança e inovação.</p>
                 </div>
                 <div className="w-full p-2">
