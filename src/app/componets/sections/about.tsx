d95b37d5bc3d079d9368d389d75b76ff349f3cc9
Sali Dev <solutti.net@gmail.com>
2024-02-06 17:59:51 -0300

    Update

--- src/app/componets/sections/about.tsx
+++ src/app/componets/sections/about.tsx
@@ -1,7 +1,31 @@
+import aboutData from "@/app/data/about";
+
+
 export function QuemSomos(){
     return(
         <div className="min-h-screen w-full flex items-center justify-center bg-gray-dark" id="quemSomos">
-            Quem Somos
+            <div className=" center flex flex-1">
+                {/*QUEM SOMOS*/}
+                <div>
+
+                </div>
+                {/*O QUE FAZEMOS*/}
+                <div>
+
+                </div>
+                {/*POR QUE ESCOLHER SOLUTTI NETWORKS?*/}
+                <div className="flex flex-1 flex-row justify-center items-center">
+                    <div className="w-[100%] lg:w-[50%] p-2 space-y-2">
+                        <div className=" text-lilas font-sans font-medium text-4xl normal-case">Por que escolher</div>
+                        <div className=" text-lilas font-sans font-bold text-4xl capitalize">solutti networks?</div>
+                    </div>
+                    <div className="w-[100%] lg:w-[50%] p-2 bg-green">
+                        {aboutData.map((items) =>
+                            <div key={items.id} >{items.name}</div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/app/data/about.ts
@@ -0,0 +1,30 @@
+const aboutData = [
+  // About
+  {
+    id: '01', // Id
+    name: 'Confiabilidade e qualidade:', // Título
+    description: 'Equipe qualificada e experiente, garantindo atendimento rápido e eficiente.' // Descrição
+  },
+  {
+    id: '02', // Id
+    name: 'Infraestrutura moderna:', // Título
+    description: 'Uma rede robusta para conexões estáveis e seguras.' // Descrição
+  },
+  {
+    id: '03', // Id
+    name: 'Transparência e ética:', // Título
+    description: 'Relação baseada em confiança e respeito com nossos clientes.' // Descrição
+  },
+  {
+    id: '04', // Id
+    name: 'Visão de futuro e inovação:', // Título
+    description: 'Compromisso com melhoria constante e atualização de serviços.' // Descrição
+  },
+  {
+    id: '05', // Id
+    name: 'Monitoramento 24/7:', // Título
+    description: 'Centros de monitoramento, NOC e CGR garantem vigilância ininterrupta.' // Descrição
+  },
+];
+
+export default aboutData;
